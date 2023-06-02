03f10aede43d0b5475f3eeff83689a6bfc690f48
kt_hayashi <kt_hayashi@lermo.co.jp>
2023-06-02 16:50:41 +0200

    ♻️ created components of Links and Footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "@/styles/Home.module.css";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a href="https://nextjs.org/" target="_blank">
+        Vercel Home Page
+      </a>
+    </footer>
+  );
+}
