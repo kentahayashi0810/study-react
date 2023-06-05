23072887d696ea97b004b5a73687de926d6e04d6
kt_hayashi <kt_hayashi@lermo.co.jp>
2023-06-05 14:41:30 +0200

    ✨ ヘッダーの追加

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import styles from "./Header.module.css";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.link}>
+        Index
+      </Link>
+      <Link href="/about" className={styles.link}>
+        About
+      </Link>
+    </header>
+  );
+}
