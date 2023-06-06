db74b019319ba711a8868326965f56cca0fda816
kt_hayashi <kt_hayashi@lermo.co.jp>
2023-06-06 15:51:36 +0200

    ⚗️ onClickの挙動確認のためボタンを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,12 +7,22 @@ import { Header } from "@/src/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(e.target.href);
+    alert(foo);
+  };
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
 
       <Footer />
