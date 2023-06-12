84202afc6e4c9d3c5137647517d544b68d023ffe
kt_hayashi <kt_hayashi@lermo.co.jp>
2023-06-12 15:13:29 +0200

    ⚡️ useCallbackとuseEffectの第２引数

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,17 +3,21 @@ import { Inter } from "next/font/google";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [count, setCount] = useState(0);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(
+    (e) => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
