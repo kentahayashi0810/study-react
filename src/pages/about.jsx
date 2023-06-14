1d0792cd97ef3b32b7db2617bf5a3d9a7df8e870
kt_hayashi <kt_hayashi@lermo.co.jp>
2023-06-14 14:03:36 +0200

    🎨 Custom Hooksを使ってうまくまとめました

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,16 +3,39 @@ import { Inter } from "next/font/google";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
+import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function About() {
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
+
   return (
     <>
       <Head>
         <title>About Page</title>
       </Head>
       <Header />
+
+      <button onClick={handleClick}>カウントアップボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      {isShow ? <h1>{count}</h1> : null}
+
+      <hr />
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加する</button>
+      <p>{text}</p>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+
       <Main page="about" />
 
       <Footer />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,54 +3,16 @@ import { Inter } from "next/font/google";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
+import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(
-    (e) => {
-      if (count < 10) {
-        setCount((prevCount) => prevCount + 1);
-      }
-    },
-    [count]
-  );
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("５文字以内にしてください。");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じものが存在します。");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <>
@@ -58,17 +20,20 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
+
       <button onClick={handleClick}>カウントアップボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-      <button onClick={handleAdd}>追加する</button>
-      <input type="text" value={text} onChange={handleChange} />
-      <p>{text}</p>
       {isShow ? <h1>{count}</h1> : null}
+
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加する</button>
+      <p>{text}</p>
       <ul>
         {array.map((item) => {
           return <li key={item}>{item}</li>;
         })}
       </ul>
+
       <Main page="index" />
 
       <Footer />
